d7d79d68826613b50657ef152d15df68f9ff627c
Gus <101070070+Gust4v1n@users.noreply.github.com>
2026-04-22 22:08:28 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "api-com-docker"
---
+++ src/main/java/com/example/apicomdocker/ApiComDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.apicomdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiComDockerApplication.class, args);
+    }
+
+}
